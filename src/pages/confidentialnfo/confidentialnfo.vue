a22c3b05534411ceb557d597d5aff4fd7cc83596
刘晓文 <597843095@qq.com>
2024-07-11 01:27:42 +0800

    123

--- src/pages/confidentialnfo/confidentialnfo.vue
+++ src/pages/confidentialnfo/confidentialnfo.vue
@@ -47,7 +47,7 @@
         </div>
 
         <div>
-          <wd-button>选择</wd-button>
+          <wd-button @click="handleClick2">选择</wd-button>
         </div>
       </div>
 
@@ -66,7 +66,7 @@
 
 
         <div>
-          <wd-button>涉密柜分配</wd-button>
+          <wd-button @click="handleClick1" >涉密柜分配</wd-button>
         </div>
 
 
@@ -87,18 +87,144 @@
 
 
 
-
-
-
-
-
   </div>
+
+
+
+
+
+
+<!--  用户名称选择-->
+  <wd-popup v-model="show2"  @close="handleClose2">
+
+    <div class="user-box">
+
+      <div class="user-box-tit">
+        选择用户
+      </div>
+
+
+
+
+
+      <div class="user-box-con">
+        <div v-for="(item,index) in nameArr" :key="index">{{item}}</div>
+      </div>
+
+
+
+
+
+
+      <div class="smg-box-footer">
+        <wd-button>取消/Cancel</wd-button>
+        <wd-button style="width: 2.1rem">确定/Ok</wd-button>
+      </div>
+
+
+
+    </div>
+
+
+
+  </wd-popup>
+
+<!--  涉密柜分配-->
+  <wd-popup v-model="show1" @close="handleClose1">
+
+    <div class="smg-box">
+
+      <div class="smg-box-tit">
+        涉密柜分配
+      </div>
+
+      <div class="smg-box-con1">
+        <span>涉密柜：</span>
+        <i>1</i>
+        <i>2</i>
+        <i>3</i>
+        <i>4</i>
+        <i>5</i>
+        <i>6</i>
+      </div>
+
+
+      <div class="smg-box-con2">
+        <div>格号：</div>
+        <div>
+          <i v-for="(item,index) in 15" :key="index">{{item}}</i>
+        </div>
+      </div>
+
+
+
+
+      <div class="smg-box-footer">
+        <wd-button>取消/Cancel</wd-button>
+        <wd-button style="width: 2.1rem">确定/Ok</wd-button>
+      </div>
+
+
+
+    </div>
+
+
+
+  </wd-popup>
+
+
+
+
 </template>
 
 <script lang="ts" setup>
 
+  const show1 = ref<boolean>(false)
+  const show2 = ref<boolean>(false)
 
 
+  const nameArr = ref<any>([
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+    '测试1',
+  ])
+
+
+
+
+
+  function handleClick2() {
+    show2.value = true
+  }
+
+  function handleClose2() {
+    show2.value = false
+  }
+
+  function handleClick1() {
+    show1.value = true
+  }
+
+  function handleClose1() {
+    show1.value = false
+  }
 
 
 </script>
@@ -288,5 +414,179 @@
     }
 
   }
+
+
+  :deep(.wd-popup--center){
+    padding: 0 !important;
+
+  }
+
+
+
+  .user-box{
+    width:8rem ;
+    /*height: 5rem;*/
+    background: #637cb2;
+    font-size: .32rem;
+    /*border: 1px solid red ;*/
+
+    .user-box-tit{
+      color: #3b12d0;
+      background: #768cbd;
+      border-bottom: 1px solid #667daf;
+      padding: .15rem .35rem;
+      font-weight: bold;
+
+    }
+
+
+    .user-box-con{
+      /*border: 1px solid red ;*/
+      display: flex;
+      flex-direction: row;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+      justify-items: center;
+
+      box-sizing: border-box;
+      padding: .25rem .15rem;
+
+
+      div{
+        width: 1.2rem;
+        height: .58rem;
+        line-height: .58rem;
+        border: 1px solid #b2bed6;
+        background: #9ca6bf;
+        font-size: .32rem;
+        text-align: center;
+        margin-right: .33rem;
+        margin-bottom: .22rem;
+      }
+      div:nth-child(5n){
+        margin-right: 0;
+      }
+      div:nth-child(n+15){
+        margin-bottom: 0
+      }
+
+
+    }
+
+
+
+
+  }
+
+
+
+
+
+  .smg-box{
+    width:8rem ;
+    /*height: 5rem;*/
+    background: #c4cce1;
+    font-size: .32rem;
+    /*border: 1px solid red ;*/
+
+    .smg-box-tit{
+      color: #1c448c;
+      border-bottom: 2px solid #c1c7d7;
+      box-shadow: 0 .05rem .05rem #c1c7d7;
+      padding: .15rem .35rem;
+      font-weight: bold;
+
+    }
+
+    .smg-box-con1{
+      /*border: 1px solid red ;*/
+      width: auto;
+      box-sizing: border-box;
+      padding: .15rem .5rem;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      span{
+        color:#1c448c ;
+        font-weight: bold;
+      }
+      i{
+        display: inline-block;
+        width: .64rem;
+        height: .58rem;
+        line-height: .58rem;
+        border: .01rem solid #999da8;
+        background: #5a5e67;
+        text-align: center;
+        color: #fff;
+
+      }
+    }
+
+
+    .smg-box-con2{
+      /*border: 1px solid red ;*/
+      display: flex;
+      padding: .15rem .5rem;
+      /*flex-direction: row;*/
+      /*justify-content: space-between;*/
+
+      div:nth-child(1){
+        color:#1c448c ;
+        font-weight: bold;
+        padding-right:.5rem;
+      }
+      div:nth-child(2){
+        /*border: 1px solid red ;*/
+        width: 5rem;
+        text-align: center;
+        i{
+          display: inline-block;
+          width: .64rem;
+          height: .58rem;
+          line-height: .58rem;
+          border: .01rem solid #999da8;
+          background: #5a5e67;
+          text-align: center;
+          color: #fff;
+          margin-right: .33rem;
+          margin-bottom: .2rem;
+        }
+        i:nth-child(5n){
+          margin-right: 0;
+        }
+        i:nth-child(n+11){
+          margin-bottom: 0;
+        }
+
+      }
+    }
+
+
+
+
+  }
+
+
+  .smg-box-footer{
+    /*border: 1px solid red ;*/
+    display: flex;
+    flex-direction:row;
+    justify-content: space-evenly;
+    padding:.4rem 0;
+
+    :deep(.wd-button.is-medium.is-round){
+      width: 2.5rem;
+      height: .6rem;
+      border-radius: 0;
+      background: #fff;
+      font-size: .32rem;
+      color: #13398b;
+      font-weight: bold;
+    }
+
+  }
+
 
 </style>
